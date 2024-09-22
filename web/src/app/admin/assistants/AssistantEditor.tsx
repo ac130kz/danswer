e8ba4ac39f40bf37521c9a27d62a9d1bcf9d032d
pablodanswer <pablo@danswer.ai>
2024-09-21 17:56:11 -0700

    minor udpate to ui

--- web/src/app/admin/assistants/AssistantEditor.tsx
+++ web/src/app/admin/assistants/AssistantEditor.tsx
@@ -1048,6 +1048,10 @@ export function AssistantEditor({
                         Starter Messages (Optional){" "}
                       </div>
                     </div>
+                    <SubLabel>
+                      Add pre-defined messages to help users get started. Only
+                      the first 4 will be displayed.
+                    </SubLabel>
                     <FieldArray
                       name="starter_messages"
                       render={(
